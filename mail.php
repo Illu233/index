c88045a8922378a014b05d3482e2d781c7b41dea
Illu233 <84707949+Illu233@users.noreply.github.com>
2021-05-24 13:54:03 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['nume'];
+$visitor_email = $_POST['e-mail'];
+$subject = $_POST['subiect'];
+$message = $_POST['mesaj'];
+
+$email_from='info@https://illu233.github.io/index/';
+
+$email_subject='Nou email';
+
+$email_body="Nume de utilizator: $name.\n".
+"Email: $visitor_email.\n".
+"Subiect: $subject.\n".
+"Mesaj: $message.\n";
+
+$to = "tufiscosmin233@gmail.com";
+
+$headers= "From: $email_from \r\n";
+
+$headers="Raspuns: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Locatie: proiectretele.html");
+
+?>
